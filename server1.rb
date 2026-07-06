dca8b7217fb8a55eab9c29efd64f91ac380ed798
Toshihiko 19RS SHIMOKAWA <71445088+k19rs999@users.noreply.github.com>
2026-07-06 11:40:13 +0900

    PATH を一応見る

--- server1.rb
+++ server1.rb
@@ -4,10 +4,13 @@
 require 'socket'
 
 def server s
-  while line=s.gets
-    pp line
-    s.puts line
-    break if line == "\r\n"
+  cmd, path, ver = s.gets.split " "
+  if path == "/"
+    pp "INDEX"
+    s.puts "index"
+  else
+    pp "OTHER"
+    s.puts "other"
   end
   s.close
 end
@@ -18,6 +21,5 @@ loop do
   s = gs.accept
   Thread.new do
     server s
-    sleep 10
   end
 end
